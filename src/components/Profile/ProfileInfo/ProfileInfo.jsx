e053410ec8d646ea6294d1e11c7e814dd2bac8ca
mob2004 <o.moldashev@gmail.com>
2020-04-09 03:13:19 +0600

    add .descriptionBlock className and selector

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -7,7 +7,7 @@ const ProfileInfo = () => {
             <div>
                 <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'/>
             </div>
-            <div>
+            <div className={s.descriptionBlock}>
                 ava + description
             </div>
         </div>
