fcab7da324a197c84ce6dfacad9ef7e6d01dd51e
mob2004 <o.moldashev@gmail.com>
2020-04-09 02:59:36 +0600

    refactoring Profile.jsx and add a new ProfileInfo.jsx file

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg' />
-            </div>
-            <div>
-                ava + description
-        </div>
-            <MyPosts hey = 'yo'/>
+            <ProfileInfo />
+            <MyPosts hey='yo' />
         </div>
     )
 }
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './ProfileInfo.module.css';
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo;
