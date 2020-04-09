91d5e1baedf156852584b5cfb0bf5fc140414fbd
mob2004 <o.moldashev@gmail.com>
2020-04-09 20:13:08 +0600

    add array of dialogs data: name and id

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,35 +3,44 @@ import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
 const DialogItem = (props) => {
-        let path = "/dialogs/" + props.id;
+    let path = "/dialogs/" + props.id;
     return (
-        <div className = {s.dialog + ' ' + s.active}>
-            <NavLink to = {path}>{props.name}</NavLink>
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
         </div>
     )
 }
 
 const Message = (props) => {
-    return <div className = {s.message}>{props.message}</div>
+    return <div className={s.message}>{props.message}</div>
 }
+
+let dialogsData = [
+    {id: 1, name: Dimych},
+    {id: 2, name: Andrey},
+    {id: 3, name: Sveta},
+    {id: 4, name: Sasha},
+    {id: 5, name: Viktor},
+    {id: 6, name: Valera}
+]
 
 const Dialogs = (props) => {
     return (
-        <div className = {s.dialogs}>
-            <div className = {s.dialogsItems}>
-                <DialogItem name = "Dimych" id = "1"/>
-                <DialogItem name = "Andrey" id = "2"/>
-                <DialogItem name = "Sveta" id = "3"/>
-                <DialogItem name = "Sasha" id = "4"/>
-                <DialogItem name = "Viktor" id = "5"/>
-                <DialogItem name = "Valera" id = "6"/>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
             </div>
-            <div className = {s.messages}>
-                <Message message = "Hi"/>
-                <Message message = "How is it going?"/>
-                <Message message = "Yo, man!"/>
-                <Message message = "Yo!"/>
-                <Message message = "Yo"/>
+            <div className={s.messages}>
+                <Message message="Hi"/>
+                <Message message="How is it going?"/>
+                <Message message="Yo, man!"/>
+                <Message message="Yo!"/>
+                <Message message="Yo"/>
             </div>
         </div>
     )
