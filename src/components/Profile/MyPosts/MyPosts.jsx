f90b4ed37aa2bf5c4f4126b80204b24562429bec
mob2004 <o.moldashev@gmail.com>
2020-04-11 14:02:54 +0600

    refactoring by adding map method, rename postsData to posts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
-let postsData = [
+let posts = [
     {id: 1, message: "How are you?", likesCount: 15},
     {id: 2, message: "It's my first post", likesCount: 10}
 ]
 
+let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 const MyPosts = (props) => {
     debugger;
     return (
@@ -22,8 +23,7 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
+                { postsElements }
             </div>
         </div>)
 }
