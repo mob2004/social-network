2cf9b8c47b360016aefe47b872f0f365d8bea48b
mob2004 <o.moldashev@gmail.com>
2020-04-15 21:27:07 +0600

    refactoring by moving dialogs array, message array, dialogsElements and messageElements from Dialogs.jsx to index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,27 +1,6 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import DialogItem from "./DialogItem/DialogItem";
-import Message from "./Message/Message";
-
-let dialogs = [
-    {id: 1, name: "Dimych"},
-    {id: 2, name: "Andrey"},
-    {id: 3, name: "Sveta"},
-    {id: 4, name: "Sasha"},
-    {id: 5, name: "Viktor"},
-    {id: 6, name: "Valera"}
-]
-
-let messages = [
-    {id: 1, message: "Hi"},
-    {id: 2, message: "How is it going?"},
-    {id: 3, message: "Yo, man!"},
-    {id: 4, message: "Yo!"},
-    {id: 5, message: "Yo"}
-]
-
-let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-let messagesElements = messages.map(m => <Message message={m.message}/>);
+import {dialogsElements, messagesElements} from "../../index";
 
 const Dialogs = (props) => {
     return (
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import DialogItem from "./components/Dialogs/DialogItem/DialogItem";
+import Message from "./components/Dialogs/Message/Message";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +17,20 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+let dialogs = [
+    {id: 1, name: "Dimych"},
+    {id: 2, name: "Andrey"},
+    {id: 3, name: "Sveta"},
+    {id: 4, name: "Sasha"},
+    {id: 5, name: "Viktor"},
+    {id: 6, name: "Valera"}
+]
+let messages = [
+    {id: 1, message: "Hi"},
+    {id: 2, message: "How is it going?"},
+    {id: 3, message: "Yo, man!"},
+    {id: 4, message: "Yo!"},
+    {id: 5, message: "Yo"}
+]
+export let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+export let messagesElements = messages.map(m => <Message message={m.message}/>);
