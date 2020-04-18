f8b6c55e10254502f4e7c2f16c669e9f98771bed
mob2004 <o.moldashev@gmail.com>
2020-04-19 03:50:16 +0600

    refactoring of code by moving posts array to index.js

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-    
-let posts = [
-    {id: 1, message: "How are you?", likesCount: 15},
-    {id: 2, message: "It's my first post", likesCount: 10},
-    {id: 3, message: "BlaBla", likesCount: 15},
-    {id: 4, message: "DaDa", likesCount: 19}
-]
 
-
-const MyPosts = () => {
+const MyPosts = (props) => {
     //debugger;
-    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let posts = [
+    {id: 1, message: "How are you?", likesCount: 15},
+    {id: 2, message: "It's my first post", likesCount: 10},
+    {id: 3, message: "BlaBla", likesCount: 15},
+    {id: 4, message: "DaDa", likesCount: 19}
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App posts = {posts}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
