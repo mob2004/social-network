cb60694f25783088ad4770acf444ea9db5c575b1
mob2004 <o.moldashev@gmail.com>
2020-04-20 21:24:19 +0600

    add addMessage callback function, button tag and a ref in textarea tag to Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -10,7 +10,11 @@ const Dialogs = (props) => {
 
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messagesElements = props.state.messages.map(m => <Message message={m.message}/>);
-
+    let newMessagElement = React.createRef();
+    let addMessage = () => {
+        let text = newMessagElement.current.value;
+        alert(text);
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -18,6 +22,14 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+            </div>
+            <div className={s.messages}>
+                <div>
+                    <textarea ref = {newMessagElement} ></textarea>
+                </div>
+                <div>
+                    <button onClick = {addMessage} >Add message</button>
+                </div>
             </div>
         </div>
     )
