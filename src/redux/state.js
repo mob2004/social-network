6955074f81514cf1e8ac614bd6ceb07580840a77
mob2004 <o.moldashev@gmail.com>
2020-05-08 03:08:50 +0600

    refactoring of messages id numbers creating in state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -95,7 +95,7 @@ let store = {
         else if (action.type === SEND_MESSAGE) {
             let body = this._state.dialogsPage.newMessageBody;
             this._state.dialogsPage.newMessageBody = '';
-            this._state.dialogsPage.messages.push({id: 6, message: body});
+            this._state.dialogsPage.messages.push({id: this._state.dialogsPage.messages.length+1, message: body});
             this._callSubscriber(this._state);
         }
         else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
