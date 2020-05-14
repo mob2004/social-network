24129223e6ccf012fa19fbd84b642b704d4c5416
mob2004 <o.moldashev@gmail.com>
2020-05-15 00:22:19 +0600

    refactoring by adding a new StoreContext.js file, adding StoreContext.Provider and StoreContext.Consumer tags

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -8,25 +8,31 @@ import {
     updateNewMessageBodyCreator
 } from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
+import StoreContext from "../../StoreContext";
 
-const DialogsContainer = (props) => {
+const DialogsContainer = () => {
+    return <StoreContext.Consumer>
+        {
+        (store) => {
+            let state = store.getState().dialogsPage;
 
-    let state = props.store.getState().dialogsPage;
-
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
+            let onSendMessageClick = () => {
+                store.dispatch(sendMessageCreator());
+            }
+            let  onNewMessageChange= (body) => {
+                store.dispatch(updateNewMessageBodyCreator(body));
+            }
+            let removeMessage = () => {
+                //props.removeMessage();
+                store.dispatch(removeMessageActionCreator());
+            }
+            return <Dialogs sendMessage={onSendMessageClick}
+                     updateNewMessageBody={onNewMessageChange}
+                     removeMessage={removeMessage}
+                     dialogsPage={state}/>
+        }
     }
-    let  onNewMessageChange= (body) => {
-        props.store.dispatch(updateNewMessageBodyCreator(body));
-    }
-    let removeMessage = () => {
-        //props.removeMessage();
-        props.store.dispatch(removeMessageActionCreator());
-    }
-    return (<Dialogs sendMessage = {onSendMessageClick}
-                     updateNewMessageBody = {onNewMessageChange}
-                     removeMessage = {removeMessage}
-                     dialogsPage = {state}/>)
+    </StoreContext.Consumer>
 }
 
 export default DialogsContainer;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "./StoreContext";
 
 let rerenderEntireTree = (state) => {
 
     ReactDOM.render(
         <React.StrictMode>
-            <App state = {state} dispatch = {store.dispatch.bind(store)} store = {store}/>
+            <BrowserRouter>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
