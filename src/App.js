826e15d3468dd96646cc13b1a0c7b3b592248b8d
mob2004 <o.moldashev@gmail.com>
2020-05-20 23:35:16 +0600

    refactoring by adding cases in users-reducer.js and adding UsersContainer.jsx file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Users from "./components/Users/Users";
+import UsersContainer from "./components/Users/UsersContainer";
 
 const App = (props) => {
     return (
@@ -23,7 +23,7 @@ const App = (props) => {
                     <Route path='/profile'
                            render = { () => <Profile /> }/>
                     <Route path='/users'
-                           render = { () => <Users /> }/>
+                           render = { () => <UsersContainer /> }/>
                     <Route path = '/news' render = {News} />
                     <Route path = '/music' render = {Music} />
                     <Route path = '/settings' render = {Settings} />
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,30 +1,43 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
 
 // initial initialization value if state is absent
 let initialState = {
     users: [
-        {id: 1, followed: false, fullName: 'Olzhas', status: 'I am a boss', location: {city: 'Almaty', country: 'Kazakhstan'}},
+        /*{id: 1, followed: false, fullName: 'Olzhas', status: 'I am a boss', location: {city: 'Almaty', country: 'Kazakhstan'}},
         {id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: {city: 'Moscow', country: 'Russia'}},
-        {id: 3, followed: false, fullName: 'Andrew', status: 'I am a boss too', location: {city: 'Kiev', country: 'Ukraine'}},
-    ],
+        {id: 3, followed: false, fullName: 'Andrew', status: 'I am a boss too', location: {city: 'Kiev', country: 'Ukraine'}}
+    */],
     newPostText: 'SamuraiJS'
 };
 
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
         case FOLLOW:
-            let stateCopy = {
-                ...state,
+            return {
                 //users: [...state.users]
                 users: state.users.map(u => {
-                    if (u.id === action.userID) {
-                        return {...u, followed: true}
+                    if (u.id === action.userId) {
+                        return { ...u, followed: true}
                     }
+                    return u;
                 })
             }
         case UNFOLLOW:
-
+            return {
+                //users: [...state.users]
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: false}
+                    }
+                    return u;
+                })
+            }
+        case SET_USERS:
+            return {
+                ...state, users: [...state.users, ...action.users]
+            }
         default:
             return state;
     }
@@ -32,6 +45,8 @@ const usersReducer = (state = initialState, action) => {
 
 export default usersReducer;
 
-export const followAC = (userID) => ({type: FOLLOW, userID})
+export const followAC = (userId) => ({type: FOLLOW, userId})
 
-export const unfollowAC = (userID) => ({type: UNFOLLOW, userID})
+export const unfollowAC  = (userId) => ({type: UNFOLLOW, userId})
+
+export const setUsersAC = (users) => ({type: SET_USERS, users})
