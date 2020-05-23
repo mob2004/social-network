8b6f64cca803c0f82232faa0aac36dc6285aab41
mob2004 <o.moldashev@gmail.com>
2020-05-23 17:25:28 +0600

    refactoring by moving logic of Users function component to class component and adding Users link in Navbar

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,29 +3,33 @@ import styles from './users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png'
 
-let Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items);
+class Users extends React.Component {
+    constructor(props) {
+        alert('NEW');
+        super(props);
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items);
             })
-        }
     }
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
-        {
-            props.users.map(u => <div key = {u.id}>
+
+    render () {
+        return <div>
+            {/*<button onClick={this.getUsers}>Get Users</button>*/}
+            {
+                this.props.users.map(u => <div key = {u.id}>
             <span>
                 <div>
                     <img src = {u.photos.small !=null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                 </div>
                 <div>
                     {u.followed
-                        ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                        : <button onClick={() => {props.follow(u.id)}}>Follow</button> }
+                        ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
+                        : <button onClick={() => {this.props.follow(u.id)}}>Follow</button> }
                 </div>
             </span>
-            <span>
+                    <span>
                 <span>
                     <div>{u.name}</div>
                     <div>{u.status}</div>
@@ -35,9 +39,11 @@ let Users = (props) => {
                     <div>{"u.location.city"}</div>
                 </span>
             </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
+
 }
 
 export default Users;
