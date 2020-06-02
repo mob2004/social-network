1c8b6639577a880f530375383af9b12084fcef5d
mob2004 <o.moldashev@gmail.com>
2020-06-02 07:52:19 +0600

    refactoring by moving componentDidMount, onPageChanged, render functions to UserContainer.jsx. This commit has a mistake TypeError: undefined has no properties

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,25 @@
 import React from "react";
 import styles from './users.module.css';
-import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png'
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount)
-            })
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
-
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users/page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-            })
-    }
-
-    render () {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
 
         return <div>
             {/*<button onClick={this.getUsers}>Get Users</button>*/}
             <div>
                 {pages.map(p => {
-                    return <span className={this.props.currentPage === p && styles.selectedPage}
-                                 onClick={() => {this.onPageChanged(p)}}>{p}</span>
+                    return <span className={props.currentPage === p && styles.selectedPage}
+                                 onClick={(e) => {
+                                     props.onPageChanged(p)
+                                 }}>{p}</span>
                 })}
             </div>
             {
@@ -47,8 +30,8 @@ class Users extends React.Component {
                 </div>
                 <div>
                     {u.followed
-                        ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
-                        : <button onClick={() => {this.props.follow(u.id)}}>Follow</button> }
+                        ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
+                        : <button onClick={() => {props.follow(u.id)}}>Follow</button> }
                 </div>
             </span>
                     <span>
@@ -64,7 +47,6 @@ class Users extends React.Component {
                 </div>)
             }
         </div>
-    }
 
 }
 
